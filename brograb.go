b66b101d9240837df8efcd96dd90082702a5bb65
Sho0pi <itayblokh@gmail.com>
2021-02-06 13:01:56 +0200

    Create the brograb.Grabber interface that lets you iterate on browser data and grab it

---
+++ brograb.go
@@ -0,0 +1,24 @@
+package brograb
+
+// Grabber is a basic data grabbing interface. Use Next and Scan to fetch the next grabbing data.
+type Grabber interface {
+
+	// Next prepares the next fetch data for reading with the Scan method. It
+	// returns true on success, or false if there is no next result row or an error
+	// happened while preparing it. Err should be consulted to distinguish between
+	// the two cases.
+	//
+	// Every call to Scan, even the first one, must be preceded by a call to Next.
+	Next() bool
+
+	// Scan copies the current fetched data into the interface pointed at
+	// by dest.
+	Scan(dest interface{}) error
+
+	// Err returns the error, if any, that was encountered during iteration.
+	Err() error
+
+	// Close stops the iteration, and closes all open databases, connections,
+	// and files used to fetch the browser data.
+	Close() error
+}
