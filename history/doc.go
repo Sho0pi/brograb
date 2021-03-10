6708867a685b11ebdaba46eaf7827a6809ba8a0f
sho0pi <itayblokh@gmail.com>
2021-03-10 22:03:33 +0200

    Add history docs

---
+++ history/doc.go
@@ -0,0 +1,3 @@
+// Package history lets you fetch history data from all common browsers databases.
+// The New.. methods return a new HistoryGrabber that implements the Grabber interface.
+package history
